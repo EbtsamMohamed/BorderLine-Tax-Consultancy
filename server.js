d5caeba3f5f4190238c5b750044f81aa71d560c7
EbtsamMohamed <ebtsamomr72@gmail.com>
2026-06-02 01:41:17 +0300

    ebtsamomr72@gmail.com

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/send", async (req, res) => {
+  const { first_name, last_name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "ebtsamomr72@gmail.com",
+      pass: "nyix sjfk bfty mdbs",
+    },
+  });
+
+  await transporter.sendMail({
+    from: email,
+    to: "Yebtsamomr72@gmail.com",
+    subject: "New Contact Form Message",
+    text: `
+Name: ${first_name} ${last_name}
+Email: ${email}
+Message: ${message}
+    `,
+  });
+
+  res.send("Email sent");
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
