911b8219180db160538e5f96e46cd04b7c96af17
EbtsamMohamed <ebtsamomr72@gmail.com>
2026-06-02 14:40:13 +0300

    changed contact form part

--- server.js
+++ server.js
@@ -31,4 +31,6 @@ Message: ${message}
   res.send("Email sent");
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
